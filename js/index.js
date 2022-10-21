c3c99993f990776f56c6039f2642cecdc25f6e8f
daniel <davillera77@gmail.com>
2022-10-20 19:43:42 -0500

    corregir icon y color

--- js/index.js
+++ js/index.js
@@ -3,3 +3,15 @@ const btn = document.getElementById("menu")
 btn.addEventListener("click", () => {
   document.getElementById("nav").classList.toggle("nav");
 })
+
+$(function() {
+
+  var $body = $(document);
+  $body.bind('scroll', function() {
+      // "Desactivar" el scroll horizontal
+      if ($body.scrollLeft() !== 0) {
+          $body.scrollLeft(0);
+      }
+  });
+
+}); 
