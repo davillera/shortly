80229a2bb146a20aaa046db4c5d7cdd33b69e0da
daniel <davillera77@gmail.com>
2022-10-25 23:18:24 -0500

    no copia :(

--- js/index.js
+++ js/index.js
@@ -1,22 +1,13 @@
 const btn = document.getElementById("menu")
-
 btn.addEventListener("click", () => {
   document.getElementById("nav").classList.toggle("nav");
 })
 
 const btnshort = document.getElementById("short")
-const url = document.getElementById("url")
-let acopiar = document.getElementById("acopiar");
-
-btnshort.addEventListener("click", () => {
-  if(url.value == "" ){
-    urlwrong()
-  }else{
-    shorter()
-  }
-})
+let url = document.getElementById("url")
 
 let historial = []
+
 
 function shorter() {
   document.getElementById("urlwrong", "url").classList.remove("malurl");
@@ -32,6 +23,7 @@ function shorter() {
     body: raw
   };
 
+
   fetch("https://api.apilayer.com/short_url/hash", requestOptions)
 
     .then(response => response.json())
@@ -44,47 +36,73 @@ function shorter() {
         long: `${result.long_url}`,
         short: `${result.short_url}`
       })
+      let contenido = result.short_url
 
-      
 
       let div = `
       <div class="result-short"> 
         <a href="${result.long_url}">${result.long_url}</a>
         
         <div class="result-btn">
-        <a href="${result.short_url}" id="acopiar" target="_blank" >${result.short_url} </a>
+        <a href="${result.short_url}" id="" target="_blank" >${result.short_url} </a>
         
-        <button id="copiar-btn" class="copiar"> <span > Copy </span> </button></div>
+        <button id="${contenido}" class="copiar" onclick="copyToClipboard(${contenido})"> <span > Copy </span> </button></div>
       </div>`
       const resultados = document.getElementById("historial")
       resultados.innerHTML += div
 
-      const copiarbtn = document.getElementById("copiar-btn")
-      
-      copiarbtn.addEventListener("click", () =>{
-        copyToClipboard()
-      })
-      function copyToClipboard() {
-        
-        const inputFalse = document.createElement("input");
-        inputFalse.setAttribute("value", acopiar.innerHTML);
-
-        document.body.appendChild(inputFalse);
-        inputFalse.select();
-        document.execCommand("copy");
-        document.body.removeChild(inputFalse);
-
-        document.querySelector('span').innerHTML = 'Copied!';
-        document.getElementById("copiar-btn").classList.toogle("copiado");
-      };
     }))
     .catch(error => console.log('error', error));
 }
 
-function urlwrong(){
-  console.log("no link");
-  document.getElementById("urlwrong","url").classList.add("malurl");
+function copyToClipboard(contenido){
+  
+  navigator.clipboard.writeText(contenido);
 }
+
+
+btnshort.addEventListener("click", () => {
+  if (url.value == "") {
+    urlwrong()
+  } else {
+    shorter()
+  }
+})
+
+function urlwrong() {
+  console.log("no link");
+  document.getElementById("urlwrong", "url").classList.add("malurl");
+}
+
+copiar-btn.addEventListener("click", e => {
+  console.log(e);
+  console.log("copiado");
+})
+
+
+// function copyToClipboard(id_elemento) {
+//   try {
+//     const inputFalse = document.createElement("input");
+//     inputFalse.setAttribute("value", document.getElementById(id_elemento).innerHTML);
+//     document.body.appendChild(inputFalse);
+//     inputFalse.select();
+//     document.execCommand("copy");
+//     document.body.removeChild(inputFalse);
+
+    
+//     // const inputFalse = document.createElement("input")
+//     // inputFalse.setAttribute("value". document,getElementById(id_elemento.innerHTML));
+//     // document.body.appendChild(inputFalse);
+//     // inputFalse.select();
+//     // const content = document.getElementById(id_elemento);
+//     // content.select();
+//     // content.setSelectionRange(0, 99999);
+//     // document.execCommand("copy");
+//     console.log("Link copiado");
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
 
 
 
